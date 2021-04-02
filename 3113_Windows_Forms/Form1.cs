8cf212c621f6210d56618c9bb4ef033046ce4c9b
수빈 이 <ccomangi2@gmail.com>
2021-04-02 09:14:58 +0900

    초기 커밋.

---
+++ 3113_Windows_Forms/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3113_Windows_Forms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (sender.Equals(button1))
+            {
+                MessageBox.Show("첫 번째 버튼 와우!");
+            }
+            else
+            {
+                MessageBox.Show("두 번째 버튼 와우!");
+            }
+        }
+    }
+}
